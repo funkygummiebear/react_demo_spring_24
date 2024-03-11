09ed7d5584a321de00c1d77c982e269cea2bbbfa
Heather Smith <smith_heather16@columbusstate.edu>
2024-03-11 19:08:16 +0000

    new changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,9 +8,16 @@ class Body extends Component{
     }
 
     render(){
+        const increment = () => {
+            this.setState({count: this.state.count + 1})
+        }
         return(
-
-
+            <React.Fragment>
+            <button type="button" className="btn btn-dark" onClick={increment}>
+                {this.state.count}
+            </button>
+            <Nav count={this.state.count} incrementFunc={increment}/>
+            </React.Fragment>
         )
     }
 }
