070b369d4122928de32258dd65379c9e49a57c8b
Heather Smith <smith_heather16@columbusstate.edu>
2024-03-11 03:26:55 +0000

    new changes

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Body extends Component{
+    super(props){
+        this.state = {
+            count: 0
+        }
+    }
+
+    render(){
+        return(
+
+
+        )
+    }
+}
