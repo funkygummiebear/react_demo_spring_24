2f0220ce19ed8998cd2804c192f56f631c0e0190
Heather Smith <smith_heather16@columbusstate.edu>
2024-03-13 16:56:54 +0000

    new changes

--- src/components/ErrorNotFound.jsx
+++ src/components/ErrorNotFound.jsx
@@ -2,14 +2,17 @@ import { useRouteError } from "react-router-dom";
 
 function ErrorNotFound() {
   const error = useRouteError();
-  console.error(error);
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <h1>
+        Oops!
+      </h1>
       <p>
-        <i>{error.statusText || error.message}</i>
+        Sorry, an unexpected error has occurred.
+      </p>
+      <p>
+        <i>{error && (error.statusText || error.message)}</i>
       </p>
     </div>
   );
