7c3cda23bdc8eb395d2777a591c63ef5bb41a57f
Heather Smith <smith_heather16@columbusstate.edu>
2024-03-13 04:37:19 +0000

    new changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,29 +5,30 @@ class Body extends Component{
     constructor(props){
         super(props)
         this.state = {
-            count: 0
+            count: 0,
+            coffee: {}
         }
     }
 
     componentDidMount(){
         //axios api call
-        axios.get('https://xkcd.com/info.0.json')
-        .then(function (response) {
-            // handle success
-            console.log(response);
+        axios.get('https://random-data-api.com/api/coffee/random_coffee')
+        .then((response) => {
+            this.setState({coffee: response.data})
         })
         .catch(function (error) {
             console.log(error);
         })
     }
 
-    componentDidUpdate(prevProps, prevState){
-        console.log(prevState)
-        console.log(this.state)
-        //function
-    }
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log(prevState)
+    //     console.log(this.state)
+    //     //function
+    // }
 
     render(){
+        const {coffee} = this.state
         const increment = () => {
             this.setState({count: this.state.count + 1})
         }
@@ -36,6 +37,9 @@ class Body extends Component{
             <button type="button" className="btn btn-dark" onClick={increment}>
                 {this.state.count}
             </button>
+            <p>
+            {coffee.blend_name}
+            </p>
             </div> 
             )
     }
